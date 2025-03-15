d02e111dfff913c71ab59088f567e7621128b910
Janniel Tan <tanjanniel@gmail.com>
2025-03-15 13:30:40 -0700

    Initial Commit

---
+++ project5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project5
+//
+//  Created by Janniel Tan on 3/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project5/project5App.swift
@@ -0,0 +1,17 @@
+//
+//  project5App.swift
+//  project5
+//
+//  Created by Janniel Tan on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct project5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
